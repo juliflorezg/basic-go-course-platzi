5f6f3d703b07b9a1a8751fc50a345eed0f425574
Julian Florez <forezjuli08@hotmail.com>
2023-08-10 08:27:16 -0500

    add - arithmetic operations

--- src/main.go
+++ src/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // go run src/main.go
 
@@ -32,4 +35,48 @@ func main() {
 	const baseCuadrado = 12
 	areaCuadrado := baseCuadrado * baseCuadrado
 	fmt.Println(areaCuadrado)
+
+	/// operaciones aritméticas
+
+	x := 3
+	y := 23
+	result := x + y
+	fmt.Println("Suma:", result)
+	result = x - y
+	fmt.Println("Resta:", result)
+	result = x * y
+	fmt.Println("Multiplicació:n", result)
+	var division float64 = float64(x) / float64(y)
+	fmt.Println("División:", division)
+	modulo := x % y
+	fmt.Println("Modulo:", modulo)
+
+	//incremental
+	x++
+	fmt.Println("Incremental", x)
+	//decremento
+	x--
+	fmt.Println("Decremental", x)
+
+	// ejercicios:
+
+	//calcular el area de un rectangulo:
+	lado1 := 23
+	lado2 := 48
+	areaRectangulo := lado1 * lado2
+
+	fmt.Println("Area Rectángulo:", areaRectangulo)
+	//calcular el area de un trapecio:
+	base1 := 23
+	base2 := 48
+	altura := 3.5
+	areaTrapecio := (float64(base1+base2) * altura) / 2
+
+	fmt.Println("Area Trapecio:", areaTrapecio)
+
+	//calcular el area de un circulo:
+	radio := 2.8
+
+	areaCirculo := math.Pi * math.Pow(radio, 2) // radio al cuadrado o radio a la segunda potencia
+	println("Area Circulo:", areaCirculo)
 }
