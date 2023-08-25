a080bcd42a9f75ab34c609600ad0d228ae7fc0d7
Julian Florez <forezjuli08@hotmail.com>
2023-08-25 10:03:57 -0500

    add stringers code

--- src/pointers/pointers.go
+++ src/pointers/pointers.go
@@ -8,6 +8,12 @@ type Pc struct {
 	Brand   string
 }
 
+// Stringers: con esta función sobreescribimos la función String del struct, para que al imprimir el struct en pantalla, se pueda ver el resultado formateado:
+//* fmt.Println(myPc) -> Tengo 32 gb de ram, 500 gb de almacenamiento y la marca es Beelink
+func (myPC Pc) String() string {
+	return fmt.Sprintf("Tengo %d gb de ram, %d gb de almacenamiento y la marca es %s", myPC.Ram, myPC.Storage, myPC.Brand)
+}
+
 func (myPC Pc) Ping() {
 	fmt.Println(myPC.Brand, "pong")
 }
